f9a487895087ccbe3efca9e25139e55dc7ef1261
Kannan Kandappan <mail4kanna@gmail.com>
2020-08-25 11:13:27 +0530

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+function defaultTask(cb) {
+
+    const postcss = require('gulp-postcss')
+    const gulp = require('gulp')
+
+    return gulp.src('css/main.css')
+    // ...
+    .pipe(postcss([
+      // ...
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // ...
+      ]))
+    // ...
+    .pipe(gulp.dest('build/'))
+
+
+  cb();
+}
+
+exports.default = defaultTask
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	purge: [],
+	theme: {
+		screens: {
+			'xs': '320px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+		},
+		customForms: theme => ({
+			default: {
+				input: {
+					margin: undefined,
+					padding: undefined,
+					color: undefined, 
+					//background-color: undefined,
+					'&:focus': {
+						boxShadow: undefined,
+						borderColor: undefined,          
+
+					},
+				},
+			}
+		})
+	},
+	plugins: [
+	require('@tailwindcss/custom-forms'),
+	],
+	variants: {},
+}
